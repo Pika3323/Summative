ebdf6ab85ed800b028e1353217ef9be16e2ff449
Pika3323 <derekellis170@yahoo.ca>
2015-12-23 23:53:15 -0500

    Yeah, sure let's just chuck our libraries in

--- Source/Core.h
+++ Source/Core.h
@@ -1,5 +1,6 @@
 #pragma once
 
+//Allegro headers
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
@@ -7,13 +8,15 @@
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_opengl.h>
 
-
+//Standard headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <ctype.h>
 #include <math.h>
-#include "Vector2D.h"
-#include "IMath.h"
-#include "GameState.h"
-#include "UIComponent.h"
+
+//Core Engine components
+#include "Vector2D.h"		//Vector header, including some vector math
+#include "IMath.h"			//Isaac's math header
+#include "GameState.h"		//Game states
+#include "UIComponent.h"	//UI
--- Source/Engine.h
+++ Source/Engine.h
@@ -3,6 +3,7 @@
 
 #pragma once
 
+//Mouse buttons (since Allegro doesn't provide these)
 #define MOUSE_LB 1
 #define MOUSE_RB 2
 #define MOUSE_MB 3
@@ -19,10 +20,8 @@
 #define BLUE800 al_map_rgb(21, 101, 192)
 #define BLUE900 al_map_rgb(13, 71, 161)
 
+//The core headers of the Engine
 #include "Core.h"
-
-#ifndef _ENGINE
-#define _ENGINE
 
 class Engine{
 public:
@@ -73,13 +72,28 @@ public:
 	Engine(){}
 	
 private:
+	//The active game display
 	ALLEGRO_DISPLAY *display;
+
+	//Information about the active game display
 	ALLEGRO_DISPLAY_MODE disp_data;
+
+	//The current queue of events
 	ALLEGRO_EVENT_QUEUE *event_queue;
+
+	//The master timer
 	ALLEGRO_TIMER *timer;
+	
+	//A font for debugging purposes
 	ALLEGRO_FONT* debug_font;
+
+	//The mouse's current state
 	ALLEGRO_MOUSE_STATE mouse_state;
+
+	//The keyboard's current state
 	ALLEGRO_KEYBOARD_STATE keyboard_state;
+
+	//The master output buffer
 	ALLEGRO_BITMAP* master_buffer;
 
 	UIComponent* LockedComponent;
@@ -90,8 +104,8 @@ private:
 	int DisplayHeight = 720, DisplayWidth = 1280;
 };
 
+//Static variable initialization
 bool Engine::bExit = false;
 
 
 Engine* GEngine = new Engine();
-#endif
